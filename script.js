1924aeb95c362a8fb1b345c797f2b82d77e42e98
NetSoftSpace <elyorkhan1997@gmail.com>
2022-05-31 13:41:28 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+window.addEventListener("DOMContentLoaded", function(){
+    let icon = document.getElementById("icon");
+
+    icon.onclick = function(){
+        document.body.classList.toggle("dark-theme");
+        if(document.body.classList.contains("dark-theme")){
+            icon.src="img/sun.png"
+        }
+        else{
+            icon.src = "img/moon.png"
+        }
+    }
+
+
+})
